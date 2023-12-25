ebc08f4d235d369b96ad44f259b2fdd1899f528f
stackblaze <119256201+stackblaze@users.noreply.github.com>
2023-12-25 16:43:37 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
-  output: 'export',
-
-  // Add the generateBuildId function
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.GIT_HASH;
-  },
+  output: 'export'
 }
 
 module.exports = nextConfig;
