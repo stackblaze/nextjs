f4a14d74b65133ccad40bd2efefc2a11a209d095
stackblaze <119256201+stackblaze@users.noreply.github.com>
2024-02-01 17:08:10 +0000

    added buildid

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
-  output: 'export'
+  output: 'export',
+  env: {
+    NEXT_PUBLIC_BUILD_ID: nextBuildId.sync({
+     dir: __dirname,
+     describe: true
+   })
+ },
 }
 
 module.exports = nextConfig;
